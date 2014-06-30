d15bc40f778307af120d65a337be9c11e4c8ccb9
osama1225 <eng.osama12@gmail.com>
2014-06-30 17:28:59 +0200

    adding initial version from optical flow

---
+++ opencv_test2/osama_part/optical_flow.h
@@ -0,0 +1,48 @@
+/*
+ * SherifCod.h
+ *
+ *  Created on: Jun 29, 2014
+ *      Author: Mohamed
+ */
+
+#ifndef OPTICAL_FLOW_H_
+#define OPTICAL_FLOW_H_
+
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+#include <ctype.h>
+#include <vector>
+#include <math.h>
+using namespace std;
+using namespace cv;
+
+class optical_flow {
+public:
+	optical_flow();
+	virtual ~optical_flow();
+	vector<Point2f> old_frame_points;
+	vector<Point2f> new_frame_points;
+	Mat img, new_gray_img, old_gray_img;
+	bool first_frame;
+	int max_count;
+	int min_distance;
+	int block_size;
+	static const float threshold = .05;
+	int up_count, down_count, left_count, right_count, non_count;
+	string final_direction;
+	vector<int> directions;
+
+	int up_direction;
+	int down_direction;
+	int left_direction;
+	int right_direction;
+	int non_direction;
+
+	float get_distance(Point2f p1, Point2f p2);
+	string get_final_direction(Mat m1,Mat m2);
+};
+
+#endif /* SHERIFCOD_H_ */
